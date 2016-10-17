8115a2313550d4b174854bbe355cb6ee203ce13a
Sarah Lensing <sarah@mapzen.com>
2016-10-17 15:15:15 -0600

    Add GeoDataApi and autocomplete method

---
+++ places/src/main/java/com/mapzen/places/AutocompleteFilter.java
@@ -0,0 +1,7 @@
+package com.mapzen.places;
+
+/**
+ * Filter for customizing autocomplete results from {@link GeoDataApi}
+ */
+public class AutocompleteFilter {
+}
---
+++ places/src/main/java/com/mapzen/places/LatLngBounds.java
@@ -0,0 +1,7 @@
+package com.mapzen.places;
+
+/**
+ * Represents a rectangular area
+ */
+public class LatLngBounds {
+}
